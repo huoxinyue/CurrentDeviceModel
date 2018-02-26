9fbcdec1ffb1ede00dca674b218242c6d760698a
huoxinyue <xinyue.huo@gvmedia.com.cn>
2018-02-26 10:59:47 +0800

    Initial Commit

---
+++ iOSCurrentDeviceModel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSCurrentDeviceModel
+//
+//  Created by User on 2018/2/26.
+//  Copyright © 2018年 User. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
